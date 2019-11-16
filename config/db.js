c00a6dedf52ceca7a30ad0093a2c0ca8a6900d49
V.Pigalov <pratimpuls@gmail.com>
2019-11-16 23:20:15 +0200

    connectDB is created

--- config/db.js
+++ config/db.js
@@ -1,2 +1,16 @@
 const mongoose = require("mongoose");
-const config = require("config")
+const MongoClient = require("mongodb");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+	try {
+		await MongoClient.connect(db, { useUnifiedTopology: true });
+		chalk.yellow("MongoDB is connected...")
+	} catch(err) {
+		console.log(err.message);
+		process.exit(1)
+	}
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
+connectDB();
+
+/*********************Global vars *****************/
+global.chalk = require("chalk-console"); //support for red, gray, blue, cyan, white, green, yellow
+/*********************************************** */
 
 app.get('/', (req, res) => {
   res.send('API running...');
 });
 
 app.listen(PORT, () => {
-  console.log(`server is listening port :${PORT}`);
+  chalk.yellow(`server is listening port :${PORT}`);
 });
