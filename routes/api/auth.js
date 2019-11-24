518465fc5e99f1ddec78b0327a32b4b9342a819d
V.Pigalov <pratimpuls@gmail.com>
2019-11-24 23:31:31 +0200

    feat(auth middl): auth middleware created

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const auth = require('../../middlware/auth');
+const User = require('../../models/User');
 
 //@route 	Get api/auth
 //@desc		Test route
 //@access 	Public
-router.get('/', (req, res) => {
-	res.send("auth route")
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (error) {
+		res.status(500).send('Server error')
+	}
 });
 
 module.exports = router
