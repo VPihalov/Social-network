6b93455cd840c32080341fc4d848174c0cd9d83f
v.pigalev <pigalev@pronet.ua>
2019-11-25 22:23:36 +0200

    feat(profile route): profile route created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const auth = require('../../middlware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-//@route 	Get api/profile
-//@desc		Test route
-//@access 	Public
-router.get('/', (req, res) => {
-	res.send("profile route")
+//@route 	Get api/profile/me
+//@desc		Get current users profile
+//@access 	Private
+router.get('/me', /*auth,*/ async (req, res) => {
+	console.log(`req.body: `, req.body)
+	try {
+		const profile = await Profile.findOne({user: req.user.id})
+			.populate('user', ['name', 'avatar']);
+		if(!profile) {
+			res.status(400).json({msg: "There is no profile  for this user"})
+		}
+		let a;
+
+	} catch (error) {
+		chalk.error(error)
+		res.status(500).send('Server error')
+	}
 });
 
 module.exports = router
