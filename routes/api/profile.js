b9b0296d402ce75eb37468ed911d6cb5b62c5592
v.pigalev <pigalev@pronet.ua>
2019-11-26 07:58:43 +0200

    feat(profile route): profile route started created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const auth = require('../../middlware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
 
 //@route 	Get api/profile/me
 //@desc		Get current users profile
 //@access 	Private
-router.get('/me', /*auth,*/ async (req, res) => {
+router.get('/me', auth, async (req, res) => {
 	console.log(`req.body: `, req.body)
 	try {
 		const profile = await Profile.findOne({user: req.user.id})
@@ -15,12 +16,53 @@ router.get('/me', /*auth,*/ async (req, res) => {
 		if(!profile) {
 			res.status(400).json({msg: "There is no profile  for this user"})
 		}
-		let a;
+		res.json(profile)
 
 	} catch (error) {
 		chalk.error(error)
 		res.status(500).send('Server error')
 	}
 });
+
+//@route 	Post api/profile
+//@desc		create and update users profile
+//@access 	Private
+router.post('/', [auth, [
+	check('status', 'Status is required').not().isEmpty(),
+	check('skills', 'Status is required').not().isEmpty(),
+]], 
+    async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.status(400).json({errors: errors.array()})
+		}
+		const {
+			company,
+			website,
+			location,
+			bio,
+			status,
+			githubsurname,
+			skills,
+			youtube,
+			facebook,
+			twitter,
+			instagram,
+			linkedin
+		} = req.body;
+
+		//Build profile object
+		const profileFields = {};
+		profileFields.user = req.user.id;
+		if(company) profileFields.company = company;
+		if(website) profileFields.website = website;
+		if(location) profileFields.location = location;
+		if(bio) profileFields.bio = bio;
+		if(status) profileFields.status = status;
+		if(githubsurname) profileFields.githubsurname = githubsurname;
+		if(skills) profileFields.skills = skills.split(',').map(skill => skill.trim());
+		console.log(`profileFields: `, profileFields);
+		res.send("Hello")
+})
 
 module.exports = router
