31cae406543054d2a34a008bc0ae341162689420
v.pigalev <pigalev@pronet.ua>
2019-11-27 10:21:53 +0200

    feat(profile routes): get hithub profile route created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const auth = require('../../middlware/auth');
+const config = require('config');
+const request = require('request');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const { check, validationResult } = require('express-validator');
@@ -264,6 +266,30 @@ router.delete('/education/:edu_id', auth, async(req, res) => {
 		res.status(500).send("Server Error")
 	}
 });
+
+//@route 	Get api/profile/git/:username
+//@desc		Get user repos from Github
+//@access 	Public
+router.get('/github/:username', (req, res) => {
+	try {
+		const options =  {
+			uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+			method: 'GET',
+			headers: {'user-agent': 'node.js'}
+		};
+		request(options, (error, response, body) => {
+			if(error) console.log(error);
+			if(response.statusCode !== 200) {
+				return res.status(404).json({msg: 'No Github profile found'})
+			}
+			res.json(JSON.parse(body))
+
+		})
+	} catch (error) {
+		console.log(error);
+		res.status(500).send("Server Error")
+	}
+})
 
 
 module.exports = router
