c04bdc337d22bcca5434af85856485824511a979
v.pigalev <pigalev@pronet.ua>
2019-11-29 09:18:38 +0200

    feat(posts routers): router for adding post created

--- models/Profile.js
+++ models/Profile.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const ProfileSchema = new mongoose.Schema({
+const ProfileSchema = new Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
+const auth = require('../../middlware/auth');
+const User = require('../../models/User');
+const Post = require('../../models/Post');
 
-//@route 	Get api/posts
-//@desc		Test route
-//@access 	Public
-router.get('/', (req, res) => {
-	res.send("posts route")
+//@route 	Post api/posts
+//@desc		Create post route
+//@access 	Private
+router.post('/', [auth, [
+	check('text', 'Text is required').not().isEmpty(),
+]], async (req, res) => {
+	const errors = validationResult(req);
+	if(!errors.isEmpty()) {	
+		res.status(400).json({errors: errors.array()})
+	}
+	try {
+		const user = await User.findById(req.user.id);
+		chalk.red('user:',  user)
+		const newPost = new Post({
+			text: req.body.text,
+			name: user.name,
+			avatar: user.avatar,
+			user: req.user.id
+		});
+		const post = await newPost.save();
+		res.send(post)
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({msg: "Server error"})
+	}
 });
 
 module.exports = router
