14a92a4207e8817af673194da57da1fb51128269
V.Pigalov <pratimpuls@gmail.com>
2019-11-30 20:50:15 +0200

    feat(components): Landing and Navbar components created

--- models/Post.js
+++ models/Post.js
@@ -3,52 +3,52 @@ const Schema = mongoose.Schema
 
 const PostSchema = new Schema({
     user: {
-        type: Schema.Types.ObjectId,
-        ref: 'user'
+			type: Schema.Types.ObjectId,
+			ref: 'user'
     },
     text: {
-        type: String,
-        required: true
+			type: String,
+			required: true
     },
     name: {
-        type: String
+			type: String
     },
     avatar: {
-        type: String
+			type: String
     },
     likes: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'user'
-            }
-        }
+			{
+				user: {
+					type: Schema.Types.ObjectId,
+					ref: 'user'
+				}
+			}
     ],
     comments: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'user'
-            },
-            text: {
-                type: String,
-                required: true
-            },
-            name: {
-                type: String
-            },
-            avatar: {
-                type: String
-            },
-            date: {
-                type: Date,
-                default: Date.now
-            }
-        }
+			{
+				user: {
+					type: Schema.Types.ObjectId,
+					ref: 'user'
+				},
+				text: {
+					type: String,
+					required: true
+				},
+				name: {
+					type: String
+				},
+				avatar: {
+					type: String
+				},
+				date: {
+					type: Date,
+					default: Date.now
+				}
+			}
     ],
     date: {
-        type: Date,
-        default: Date.now
+			type: Date,
+			default: Date.now
     }
 });
 
