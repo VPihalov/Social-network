4aa97aa7976e34505ee6992c2c736e243392f534
V.Pigalov <pratimpuls@gmail.com>
2019-12-02 23:50:27 +0200

    feat(store): redux store created

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,26 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import './App.css';
+//Redux
+import {Provider} from 'react-redux';
+import store from './store';
 
-const App = () => 
-	<Router>
-		<Fragment>
-			<Navbar/>
-			<Route exact path="/" component={Landing}/>
-			<section className="container">
-				<Switch>
-					<Route exact path="/register" component={Register}/>
-					<Route exact path="/login" component={Login}/>
-				</Switch>
-			</section>
-		</Fragment>
-	</Router>
+const App = () => (
+	<Provider store={store}>			
+		<Router>
+			<Fragment>
+				<Navbar/>
+				<Route exact path="/" component={Landing}/>
+				<section className="container">
+					<Switch>
+						<Route exact path="/register" component={Register}/>
+						<Route exact path="/login" component={Login}/>
+					</Switch>
+				</section>
+			</Fragment>
+		</Router>
+	</Provider>	
+)
 
 
 
