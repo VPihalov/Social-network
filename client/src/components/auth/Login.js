2062183de5210879d46083fd90c2e0b8f48d55a7
V.Pigalov <pratimpuls@gmail.com>
2019-12-02 22:33:24 +0200

    feat(Register component): register component created

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
+	//formData - our state - all widgets on the form
+	//formDate is like state = {formData: {}}
+	//setFormData - function to update state
+	//setFormData - is like this.setState - pass new state
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
+
+	const {email, password} = formData;
+	const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+	const onSubmit = async e => {
+		e.preventDefault();
+		console.log('Sing in success')
+	}
+	
 	return (
-		<div>
-			Login
-		</div>
+		<Fragment>
+			<h1 className="large text-primary">Sign in</h1>
+				<p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+				<form className="form" onSubmit={e => onSubmit(e)}>
+					<div className="form-group">
+						<input
+							type="email" 
+							placeholder="Email Address" 
+							name="email"
+							value={email} 
+							onChange={e => onChange(e)} 
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							placeholder="Password"
+							name="password"
+							value={password} 
+							onChange={e => onChange(e)} 
+							required
+							minLength="6"
+						/>
+					</div>
+					<div className="form-group">
+					</div>
+					<input type="submit" className="btn btn-primary" value="Register" />
+				</form>
+				<p className="my-1">
+					Don't have an account yet? <Link to="/register">Sign In</Link>
+				</p>
+		</Fragment>
 	)
 };
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
-import React, {Fragment, useState} from 'react'
-import { set } from 'mongoose';
+import React, {Fragment, useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Register = () => {
 	//formData - our state - all widgets on the form
@@ -15,12 +15,12 @@ const Register = () => {
 
 	const {name, email, password, password2} = formData;
 	const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
-	const onSubmit = e => {
+	const onSubmit = async e => {
 		e.preventDefault();
 		if(password !== password2) {
 			console.log("Passwords do not match");
 		} else {
-			console.log(formData)
+			console.log("User created");
 		}
 	}
 	
@@ -78,7 +78,7 @@ const Register = () => {
 					<input type="submit" className="btn btn-primary" value="Register" />
 				</form>
 				<p className="my-1">
-					Already have an account? <a href="login.html">Sign In</a>
+					Already have an account? <Link to="/login">Sign In</Link>
 				</p>
 		</Fragment>
 	)
