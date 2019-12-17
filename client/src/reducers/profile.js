26b865172bfd6beaa22231ad9b0bd36330a12707
V.Pigalov <pratimpuls@gmail.com>
2019-12-18 00:01:39 +0200

    feat(dashboard): get profile action created

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,16 @@
+
+const initialState = {
+	profile: null,
+	profiles: [],
+	repos: [],
+	loading: true,
+	error: {}
+};
+
+export default function (state = initialState, action) {
+	const {type, payload} = action;
+
+	switch(type) {
+		case:
+	}
+}
