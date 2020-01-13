f4fae0e1ef0b0bb44b731c38b7dae637c75a1365
V.Pigalov <pratimpuls@gmail.com>
2020-01-13 22:48:26 +0200

    feat(dashboard): dashboard created

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,29 +1,25 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {getCurrentProfile} from '../../actions/profile';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = ({getCurrentProfile, auth, profile}) => {
-	useEffect(() => {
-		getCurrentProfile()
-	}, [])
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
 
-	return (
-		<div>
-			Dashboard
-		</div>
-	)
+  return <div>Dashboard</div>;
 };
 
 const mapStateToProps = state => ({
-	auth: state.auth,
-	profile: state.profile,
+  auth: state.auth,
+  profile: state.profile
 });
 
 Dashboard.propTypes = {
-	getCurrentProfile: PropTypes.func.isRequired,
-	auth: PropTypes.object.isRequired,
-	profile: PropTypes.object.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 };
 
-export default connect(mapStateToProps, {getCurrentProfile}) (Dashboard)
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
