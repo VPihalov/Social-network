9e34753c65681020769b8ab6c614ef5f346cc98d
v.pigalev <pigalev@pronet.ua>
2020-01-31 23:08:52 +0200

    feat(components): DashboardActions created

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import Spinner from '../layout/Spinner';
 import {getCurrentProfile} from '../../actions/profile';
+import DashboardActions from './DashboardActions';
 
 const Dashboard = ({
 	getCurrentProfile,
@@ -20,7 +21,11 @@ const Dashboard = ({
 			<i className="fas fa-user"></i>
 			{' '}Welcome {user && user.name}
 		</p>
-{profile !== null ? (<Fragment>has </Fragment>) : (<Fragment>
+{profile !== null ? (
+	<Fragment>
+		<DashboardActions/>
+	</Fragment>
+		) : (<Fragment>
 		<p>You have not yet setup a profile, please add some info</p>
 		<Link to='/create-profile' className="btn btn-primary my-1">
 			Create Profile
