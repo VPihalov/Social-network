7e90bd18a8675cc5ebe5ce41581ca746fd2307ac
V.Pigalov <pratimpuls@gmail.com>
2020-03-01 00:38:26 +0200

    server modified

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'src', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
