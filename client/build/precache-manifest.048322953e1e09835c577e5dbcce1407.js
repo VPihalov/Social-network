3f2ac78f740ccf9da4a7b83acf7b2c3d9195ad29
V.Pigalov <pratimpuls@gmail.com>
2020-03-01 00:35:14 +0200

    build file created

---
+++ client/build/precache-manifest.048322953e1e09835c577e5dbcce1407.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22de004cbc5a68d663426ebf0de550e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "badd7424f9f5be3a2fa0",
+    "url": "/static/css/main.e6696cec.chunk.css"
+  },
+  {
+    "revision": "9b7d67e0ced95a47af17",
+    "url": "/static/js/2.d5b66f8e.chunk.js"
+  },
+  {
+    "revision": "badd7424f9f5be3a2fa0",
+    "url": "/static/js/main.aef9e14a.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "2096339a09096db2cd3cdb64401fd25f",
+    "url": "/static/media/showcase.2096339a.jpg"
+  },
+  {
+    "revision": "0c194e0388774f957906aa97d5d0be50",
+    "url": "/static/media/spinner.0c194e03.gif"
+  }
+]);
