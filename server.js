bb0c4a97c4affced9fa128a61b85ed152ada88c7
V.Pigalov <pratimpuls@gmail.com>
2020-03-01 22:14:25 +0200

    prepared for deployment

--- server.js
+++ server.js
@@ -18,12 +18,15 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+	// Set static folder
+	console.log("process.env.NODE_ENV === 'production'")
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
+} else {
+	console.log("something went wrong")
 }
 
 const PORT = process.env.PORT || 5000;
